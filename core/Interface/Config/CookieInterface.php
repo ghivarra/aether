7e6adbdf525397d6e3144f8b56a154df089e69e5
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-06-10 11:48:16 +0700

    done adding cookie interface

--- core/Interface/Config/CookieInterface.php
+++ core/Interface/Config/CookieInterface.php
@@ -9,5 +9,11 @@
 
 interface CookieInterface
 {
-    
+    public string $prefix { get; set; }
+    public int $expires { get; set; }
+    public string $path { get; set; }
+    public string $domain { get; set; }
+    public bool $secure { get; set; }
+    public bool $httponly { get; set; }
+    public string $samesite { get; set; }
 }
