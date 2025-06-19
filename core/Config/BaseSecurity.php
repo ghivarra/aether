17c31a32ab851b31f9829212854b173816b0e2e4
Ghivarra Senandika Rushdie <gsenandika@gmail.com>
2025-06-19 21:54:33 +0700

    fix Config\Security base

--- core/Config/BaseSecurity.php
+++ core/Config/BaseSecurity.php
@@ -7,13 +7,38 @@ namespace Aether\Config;
 use Aether\Interface\Config\SecurityInterface;
 
 /** 
- * Base Security Configurations
+ * Base App Configurations
  * 
  * @class Aether\Config
  * 
 **/
 
-class BaseSecurity implements SecurityInterface
+class BaseSecurity extends BaseConfig implements SecurityInterface
 {
-    
+    public function __construct(array|null $config = null)
+    {
+        // set default
+        $this->tokenName = getDotEnv('App.tokenName', 'string', $this->tokenName);
+        $this->headerName = getDotEnv('App.headerName', 'string', $this->headerName);
+        $this->cookieName = getDotEnv('App.cookieName', 'string', $this->cookieName);
+        $this->expires = getDotEnv('App.expires', 'int', $this->expires);
+        $this->regenerate = getDotEnv('App.regenerate', 'bool', $this->regenerate);
+        $this->redirect = getDotEnv('App.redirect', 'bool', $this->redirect);
+
+        if (!is_null($config))
+        {
+            $this->rewriteConfig($config);
+        }
+    }
+
+    //==================================================================
+
+    public string $tokenName = 'X_CSRF_TOKEN';
+    public string $headerName = 'X-CSRF-TOKEN';
+    public string $cookieName = 'X_CSRF_COOKIE';
+    public int $expires = 7200;
+    public bool $regenerate = true;
+    public bool $redirect = false;
+
+    //==================================================================
 }
--- core/Interface/Config/SecurityInterface.php
+++ core/Interface/Config/SecurityInterface.php
@@ -2,5 +2,10 @@
 
 interface SecurityInterface
 {
-    
+    public string $tokenName { get; set; }
+    public string $headerName { get; set; }
+    public string $cookieName { get; set; }
+    public int $expires { get; set; }
+    public bool $regenerate { get; set; }
+    public bool $redirect { get; set; }
 }
